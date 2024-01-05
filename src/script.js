5f29cdc143555901e1133c0d6f4398b9e184eb7e
Nathan Clive Rego <nathan.crego@outlook.com>
2024-01-05 12:01:56 +0530

    implemented scientific functions.

--- src/script.js
+++ src/script.js
@@ -11,7 +11,19 @@ document.addEventListener("DOMContentLoaded", function () {
     function evaluateResult()
     {
         const convertedValue = currentValue
-        .replace('%','*0.01');
+        .replace('%','*0.01')
+        .replace('log','Math.log10')
+        .replace('sin','Math.sin')
+        .replace('cos','Math.cos')
+        .replace('tan','Math.tan')
+        .replace('asin','Math.asin')
+        .replace('acos','Math.acos')
+        .replace('atan','Math.atan')
+        .replace('Π','Math.PI')
+        .replace('√','Math.sqrt')
+        .replace('e','Math.exp')
+        .replace('ln','Math.log')
+        .replace('x^y','Math.pow')
         const result = eval(convertedValue) //new use eval in production environment as it just evaluates any value present in the code.
         currentValue = result.toString();
         display.value = currentValue;
@@ -22,8 +34,9 @@ document.addEventListener("DOMContentLoaded", function () {
         const button = buttons[i];
         button.addEventListener('click',function(){
             const value = button.innerText;
-
-            if(value == "AC")
+            try
+            {
+                if(value == "AC")
             {
                 currentValue = "";
                 display.value = currentValue;
@@ -38,6 +51,13 @@ document.addEventListener("DOMContentLoaded", function () {
                 display.value = currentValue;
             }
 
+            }
+            catch(error)
+            {
+                console.error(error);
+                currentValue = "ERROR";
+                display.value = currentValue;
+            }
         })
     }
 });
